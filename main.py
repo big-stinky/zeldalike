e9a16f3f3a306159791d4fbc73e1aef2c9876c21
trolldruid <elijahkent99@gmail.com>
2019-01-17 20:28:06 -0400

    Added Movement

--- main.py
+++ main.py
@@ -24,21 +24,41 @@ def main():
 	#main game loop
 	done = False
 	while not done:
+		clock.tick(60)
 		screen.fill(black)
 		sprites.draw(screen)
 
-		pygame.display.flip()
 
-		for e in pygame.event.get():
-			if e.type == QUIT or (e.type == KEYUP and e.key == K_ESCAPE):
+		for event in pygame.event.get():
+			if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
 				done = True
 				break
 
-			if e.type == KEYUP:
-				if e.key == K_RIGHT:
-					player.rect.x += 10
+			if event.type == KEYDOWN:
+				if event.key == K_RIGHT:
+					player.move(2,0)
+				if event.key == K_LEFT:
+					player.move(-2,0)
+				if event.key == K_UP:
+					player.move(0,-2)
+				if event.key == K_DOWN:
+					player.move(0,2)
+				
+
+			if event.type == KEYUP:
+				if event.key == K_RIGHT:
+					player.move(-2,0)
+				if event.key == K_LEFT:
+					player.move(2,0)
+				if event.key == K_UP:
+					player.move(0,2)
+				if event.key == K_DOWN:
+					player.move(0,-2)
+				
 		
-		clock.tick(60)
+		player.update()
+
+		pygame.display.flip()
 
 if __name__ == '__main__':
 	main()
